f432a72c6953c579544aac57072edc6b359b0a5a
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-04-30 04:00:34 +0530

    logic add

--- Weather-app/script.js
+++ Weather-app/script.js
@@ -1,1 +1,21 @@
-// https://api.openweathermap.org/data/2.5/weather?q=german&appid=66a59b78e7600456b6c73e2cccf0945c&units=metric
+const apiKey = "66a59b78e7600456b6c73e2cccf0945c";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
