5db4bae53542edbbf7bd23d6df1cca52a9568cc6
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-05-02 06:27:00 +0530

    add question

---
+++ Quiz-app/script.js
@@ -0,0 +1,60 @@
+const questions = [
+  {
+    question:
+      "Speed is a scalar quantity. What is the corresponding vector quantity?",
+    answer: [
+      { text: "Momentum", correct: false },
+      { text: "Acceleration", correct: false },
+      { text: "Velocity", correct: true },
+      { text: "Displacement", correct: false },
+    ],
+  },
+  {
+    question: "What device is used to measure electric current?",
+    answer: [
+      { text: "Voltmeter", correct: false },
+      { text: "Ammeter", correct: true },
+      { text: "Barometer", correct: false },
+      { text: "Thermometer", correct: false },
+    ],
+  },
+  {
+    question: "What is the speed of light in a vacuum?",
+    answer: [
+      { text: "3,000 km/s", correct: false },
+      { text: "30,000 km/s", correct: false },
+      { text: "300,000 km/s", correct: true },
+      { text: "3,000,000 km/s", correct: false },
+    ],
+  },
+  {
+    question:
+      "What happens to the resistance of a conductor as its temperature increases?",
+    answer: [
+      { text: "Decreases", correct: false },
+      { text: "Remains the same", correct: false },
+      { text: "Increases", correct: true },
+      { text: "Becomes zero", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which fundamental force is responsible for holding electrons around the nucleus?",
+    answer: [
+      { text: "Gravitational Force", correct: false },
+      { text: "Electromagnetic Force", correct: true },
+      { text: "Strong Nuclear Force", correct: false },
+      { text: "Weak Nuclear Force", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which phenomenon causes a straw to appear bent in a glass of water?",
+    answer: [
+      { text: "Reflection", correct: false },
+      { text: "Refraction", correct: true },
+      { text: "Diffraction", correct: false },
+      { text: "Dispersion", correct: false },
+    ],
+  },
+];
